bac9bb24f2e71ec74243ff3f2f94e575c5abcd3d
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-09-12 15:24:22 +0100

    added data and title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios';
 
-const nasaApi = 'https://api.nasa.gov/api.html#apod';
+const nasaApi = 'https://lambda-github-api-server.herokuapp.com';
 
 function App() {
+
+//Title
+const [displayTitle, setDisplayTitle] = useState({
+  data: []});
+
+  useEffect(() => {
+    axios.get(nasaApi)
+      .then(response =>{
+        // debugger
+        setDisplayTitle({
+          data: response.data.title
+        });
+        // console.log(response.data.explanation);
+      })
+      .catch(error => {
+
+      })
+  }, [])
+//Date
+const [displayDate, setDisplayDate] = useState({
+  data: []});
+
+  useEffect(() => {
+    axios.get(nasaApi)
+      .then(response =>{
+        debugger
+        setDisplayDate({
+          data: response.data.date
+        });
+        // console.log(response.data.explanation);
+      })
+      .catch(error => {
+
+      })
+  }, [])
+
+
   return (
     <div className="App">
       <p>
